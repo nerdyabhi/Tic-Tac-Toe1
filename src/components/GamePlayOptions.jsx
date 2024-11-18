b5556cb02655f28bd635606986113bf336116a18
Abhi Sharma <me.avish143@gmail.com>
2024-11-18 20:05:39 +0530

    Fixed bug

--- src/components/GamePlayOptions.jsx
+++ src/components/GamePlayOptions.jsx
@@ -31,7 +31,7 @@ const GamePlayOptions = ({resetGame , aiMode , SetAIMode , gameMode , setGameMod
     return (
         <>
         
-            <div className="flex flex-row gap-4 ">
+            <div className="flex flex-row gap-4 z-10 ">
                     {aiMode && !gameMode&&  <GameMode setGameMode={setGameMode}/>}
                     <button className='border px-3 w-[120px ] py-2 btn bg-opacity-0 hover:bg-opacity-0 rounded-xl border-slate-500  transition-all duration-200  shadow-md shadow-blue-800 flex justify-center items-center' onClick={()=>resetGame()}>Reset Game <kbd className="kbd kbd-sm">R</kbd></button>
                     <button className='border px-3 w-[120px ] py-2 btn bg-opacity-0 hover:bg-opacity-0 rounded-xl border-slate-600  transition-all duration-200  shadow-md shadow-blue-800 flex justify-center items-center' onClick={()=>{resetGame() , SetAIMode(!aiMode) }}>{aiMode?"With Friend":"VS AI"} <kbd className="kbd kbd-sm">A</kbd></button>
