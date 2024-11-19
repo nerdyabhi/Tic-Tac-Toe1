f2d5b5e377964d8270921ebabadcb330f08d204f
Abhi Sharma <me.avish143@gmail.com>
2024-11-19 23:26:09 +0530

    Updated winscreen

--- src/components/WinScreen.jsx
+++ src/components/WinScreen.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Trophy, RefreshCw, RotateCcw } from 'lucide-react';
+import React, { useEffect } from 'react';
 
 const WinningGif = ({ Winner }) => {
-    const gifSrc = Winner === "Draw"
+    console.log(Winner);
+    
+    const gifSrc = Winner === "Game draw."
         ? "https://nerdyabhi.github.io/tic-tac-toe/asset/memeGameDraw.gif"
         : Winner === "X" ? "https://nerdyabhi.github.io/tic-tac-toe/asset/memeWin.webp" : "https://nerdyabhi.github.io/tic-tac-toe/asset/machineWins.webp";
 
@@ -10,35 +11,63 @@ const WinningGif = ({ Winner }) => {
         <img
             src={gifSrc}
             alt="Game Result"
-            className="md:w-64 md:h-64 w-32 h-32 mb-6  rounded-lg "
+            className="w-48 h-48 mb-6  rounded-lg "
         />
     );
 };
 
-const WinScreen = ({ Winner, resetGame }) => {
+const checkPrevgame = (Winner , setWinner , SetDisable)=>{
+    if(!Winner) return;
+    setWinner(null);
+
+    SetDisable(true);
+}
+
+
+const WinScreen = ({ Winner, resetGame  , setWinner , SetDisable}) => {
+    let [score1 , score2] = [0  , 0];
+
+    const HandleKeyPress = (e)=>{
+        if(e.key.toLowerCase() == "g"){
+            checkPrevgame(Winner , setWinner , SetDisable);
+            return;
+        }
+
+        if(e.key.toLowerCase() == "n"){
+            if(!Winner) return;
+            resetGame();
+        }
+    }
+
+    if(Winner == "X") score1++;
+    else if(Winner == "O") score2++;
+
+
+    useEffect(()=>{
+        window.addEventListener("keydown" , (e)=> HandleKeyPress(e));
+        return ()=>window.removeEventListener("keydown" , HandleKeyPress);
+    } , [Winner])
+
     return (
-        <div className="flex flex-col  w-[100vw] h-[100vh] justify-center items-center min-h-screen bg-zinc-900 absolute">
-          
-            <div className="relative  flex flex-col items-center text-center space-y-8 p-12 rounded-2xl z-10 bg-slate-900 ">
-            
-                {/* Game result announcement */}
-                <h1 className="text-5xl font-bold bg-gradient-to-r from-blue-400 via-white to-blue-400 bg-clip-text text-transparent tracking-wider">
-                    {Winner === "Draw" ? "It's a Draw!" : `${Winner} Wins!`}
-                </h1>
+        <div className='flex flex-col  absolute h-[100vh] w-[100vw] bg-black z-20 bg-opacity-80 items-center justify-center'>
+                <div className="container flex flex-col items-center justify-center gap-5">
+                    {Winner!=="Game draw." &&<h1 className=' font-mono text-3xl font-bold '> <span className={Winner==="X"?"text-red-500":"text-green-500"}>{Winner}</span> Won the match 🎉🥳</h1>}
+                    {Winner=="Game draw." &&<h1 className=' font-mono text-3xl font-bold '> It's a Draw ! </h1>}
+                <WinningGif Winner={Winner}/>
+                  </div>
+                  <h1 className='2xl  font-bold font-mono'>Scores : </h1>
+                    <div className="mb-2">
+                        <h1 className='text-xl text-red-500 font-bold'>X - {score1}</h1>
+                        <h1 className='text-xl text-green-500 font-bold'>O - {score2}</h1>
+                    </div>
+                    <div className="buttons space-y-3">
+                    <button className='border px-3 w-[120px ] py-2 btn bg-opacity-0 hover:bg-opacity-0 rounded-xl w-[200px] max-w-[300px] border-slate-500  transition-all duration-200  shadow-md shadow-blue-800 flex justify-center items-center' onClick={()=>resetGame()}>New Game <kbd className="kbd kbd-sm">N</kbd></button>
+                    <button className='border px-3 w-[120px ] py-2 btn bg-opacity-0 hover:bg-opacity-0 rounded-xl w-[200px] max-w-[300px] border-slate-500  transition-all duration-200  shadow-md shadow-blue-800 flex justify-center items-center' onClick={()=>checkPrevgame(Winner , setWinner , SetDisable)}>Check Previous Game <kbd className="kbd kbd-sm">G</kbd></button>
+                    </div>
 
-            
-
-                <WinningGif Winner={Winner} />
-
-                {/* Game control buttons */}
-                <div className="flex gap-6 justify-center items-center">
-                <button className="btn w-[225px] border-2 border-white hover:scale-105 transition-all duration-200" onClick={resetGame} >Play Again</button>
-                <button className="btn w-[225px] border-2 border-white hover:scale-105 transition-all duration-200" onClick={resetGame} >Play Again</button>
-                  
-                </div>
-            </div>
-
-           </div>
+                {/* </div> */}
+                
+        </div>
     );
 };
 
